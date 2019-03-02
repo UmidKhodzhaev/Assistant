9c7cb844f6247099b527ec65edf904ae023bf98a
UmidKhodzhaev <umidik3@gmail.com>
2019-03-03 02:30:24 +0700

    Some updates
    OnClickListener

--- app/src/main/java/com/example/assistant/MainActivity.java
+++ app/src/main/java/com/example/assistant/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         micro = findViewById(R.id.imageView2);
+        micro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
